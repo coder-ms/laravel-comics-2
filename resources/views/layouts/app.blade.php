2f5036376565655a2e60bdbf4aaab5c460c589b7
coder-ms <marco.sbrascia@gmail.com>
2022-12-21 16:00:29 +0200

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,33 @@
+
+<?php
+$footerSearch = config('dbDCComics'); //.footerSearch
+?>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>@yield('pageTitle', 'Home Page')</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    @vite('resources/js/app.js')
+
+</head>
+
+<body>
+   {{--@include('partials.banner')--}}
+    @include('partials.header')
+    {{--@include('partials.jumbotron')--}}
+    <main>
+       {{--@yield('currentSeries')--}}
+       <h1>HELLO WORLD</h1>
+    </main>
+    @include('partials.footer') {{--, compact('footerSearch') )--}}
+</body>
+
+</html>
